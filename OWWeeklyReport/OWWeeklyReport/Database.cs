96979f97b8c275d106677a4946b8d232b1cdb96a
_nebula <41904486+misternebula@users.noreply.github.com>
2023-03-01 17:50:34 +0000

    uhhh everything

---
+++ OWWeeklyReport/OWWeeklyReport/Database.cs
@@ -0,0 +1,16 @@
+﻿namespace OWWeeklyReport;
+
+public class Database
+{
+	public Release[] releases { get; set; }
+}
+
+public class Release
+{
+	public string name { get; set; }
+	public string uniqueName { get; set;}
+	public string repo { get; set; }
+	public int downloadCount { get; set; }
+	public string slug { get; set; }
+	public string[] tags { get; set; }
+}
--- OWWeeklyReport/OWWeeklyReport/Program.cs
+++ OWWeeklyReport/OWWeeklyReport/Program.cs
@@ -1,56 +1,241 @@
-﻿using System.Diagnostics;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Diagnostics;
+using System.Net;
 using System.Text.Json;
 
 namespace OWWeeklyReport;
 
-internal class Program
+public class Program
 {
-	static void Main(string[] args)
+	public const int NUMBER_OF_MODS = 10;
+
+	public static void Main(string[] args)
+	{
+		Task.WaitAll(new Program().MainAsync(args));
+	}
+
+	public List<Entry> DownloadList = new();
+
+	public (int, Dictionary<string, int>) AnalyzeMods(DateTime fromTime, DateTime toTime)
+	{
+		Dictionary<string, int> downloadChanges = new();
+		var numberOfNewMods = 0;
+
+		foreach (var entry in DownloadList)
+		{
+			if (entry.Repo == "IGNORE_ENTRY"
+				|| entry.Updates == null
+				|| entry.Updates.Length == 0)
+			{
+				continue;
+			}
+
+			var onlyInTimeSpan = entry.Updates.Where(x => UnixTimeStampToDateTime(x.UnixTimestamp) >= fromTime && UnixTimeStampToDateTime(x.UnixTimestamp) <= toTime);
+
+			if (!onlyInTimeSpan.Any())
+			{
+				continue;
+			}
+
+			var ordered = onlyInTimeSpan.OrderByDescending(x => x.UnixTimestamp).ToList();
+
+			// filter outliers
+			List<DownloadCountUpdate> newOrdered = new();
+			for (var i = 0; i < ordered.Count; i++)
+			{
+				var currentValue = ordered[i];
+
+				if (i == ordered.Count - 1)
+				{
+					newOrdered.Add(currentValue);
+					continue;
+				}
+
+				var nextValue = ordered[i + 1];
+
+				if (currentValue.DownloadCount - nextValue.DownloadCount > 100)
+				{
+					i += 2;
+				}
+				else
+				{
+					newOrdered.Add(currentValue);
+				}
+			}
+
+			var newest = newOrdered.First();
+			var oldest = newOrdered.Last();
+
+			if (UnixTimeStampToDateTime(oldest.UnixTimestamp).DayOfWeek != fromTime.DayOfWeek && oldest.DownloadCount == 0)
+			{
+				numberOfNewMods++;
+			}
+
+			downloadChanges.Add(entry.Repo, newest.DownloadCount - oldest.DownloadCount);
+		}
+
+		return (numberOfNewMods, downloadChanges);
+	}
+
+	public async Task MainAsync(string[] args)
 	{
 		//var downloadCountFile = File.ReadAllText(args[0]);
-		var downloadCountFile = File.ReadAllText(@"C:\Users\hpoin\Downloads\download-history.json");
-		var json = JsonSerializer.Deserialize<List<Entry>>(downloadCountFile);
 
-		if (json == null)
+		//var discordToken = args[1];
+
+		var downloadCountFile = "";
+		using (var hc = new HttpClient())
+		{
+			downloadCountFile = await hc.GetStringAsync(@"https://raw.githubusercontent.com/ow-mods/ow-mod-download-history/main/download-history.json");
+		}
+		DownloadList = JsonSerializer.Deserialize<List<Entry>>(downloadCountFile);
+
+		if (DownloadList == null)
 		{
 			return;
 		}
 
-		var currentTime = DateTime.Now;
+		(var numberOfNewMods, var downloadChanges) = AnalyzeMods(DateTime.Now.AddDays(-7), DateTime.Now);
+		(var numberOfNewModsLastWeek, var downloadChangesLastWeek) = AnalyzeMods(DateTime.Now.AddDays(-14), DateTime.Now.AddDays(-7));
 
-
-
-		foreach (var entry in json)
+		var modsToIgnore = new string[]
 		{
-			if (entry.Repo == "IGNORE_ENTRY")
-			{
-				continue;
-			}
+			"https://github.com/ow-mods/ow-mod-manager",
+			"https://github.com/ow-mods/owml",
+			"https://github.com/amazingalek/owml"
+		};
 
-			if (entry.Updates == null || entry.Updates.Length == 0)
-			{
-				continue;
-			}
-
-			var orderedByTimestamp = entry.Updates.OrderByDescending(x => x.UnixTimestamp);
-
-			var latestDownloadCount = orderedByTimestamp.First().DownloadCount;
-
-			var oldestDownloadCount = 0;
-
-			if ((currentTime - UnixTimeStampToDateTime(orderedByTimestamp.Last().UnixTimestamp)).TotalDays > 7)
-			{
-				oldestDownloadCount = orderedByTimestamp.Last(x => (currentTime - UnixTimeStampToDateTime(x.UnixTimestamp)).TotalDays <= 7).DownloadCount;
-			}
-
-			Console.WriteLine($"{entry.Repo} went from {oldestDownloadCount} to {latestDownloadCount}");
+		var database = "";
+		using (var hc = new HttpClient())
+		{
+			database = await hc.GetStringAsync(@"https://ow-mods.github.io/ow-mod-db/database.json");
 		}
+		var databaseJson = JsonSerializer.Deserialize<Database>(database);
+
+		bool IsValidMod(string repo)
+		{
+			if (modsToIgnore.Contains(repo))
+			{
+				return false;
+			}
+
+			var modDbEntry = databaseJson.releases.FirstOrDefault(x => x.repo == repo);
+
+			if (modDbEntry == null)
+			{
+				// :HUHH:
+				return false;
+			}
+
+			if (modDbEntry.tags.Contains("library"))
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		var orderedTW = downloadChanges.Where(x => IsValidMod(x.Key)).OrderByDescending(x => x.Value);
+		var topTenTW = orderedTW.Take(NUMBER_OF_MODS);
+
+		var orderedLW = downloadChangesLastWeek.Where(x => IsValidMod(x.Key)).OrderByDescending(x => x.Value);
+		var topTenLW = orderedLW.Take(NUMBER_OF_MODS);
+
+		var topTenLWRepos = topTenLW.Select(x => x.Key).ToList();
+
+		var topTen = new List<(string repo, int change, int oldIndex)>();
+
+		for (var i = 0; i < topTenTW.Count(); i++)
+		{
+			var repo = topTenTW.ElementAt(i).Key;
+
+			var oldIndex = -1;
+			if (topTenLW.Any(x => x.Key == repo))
+			{
+				oldIndex = topTenLWRepos.IndexOf(repo);
+			}
+
+			topTen.Add((repo, topTenTW.ElementAt(i).Value, oldIndex));
+		}
+
+		var client = new DiscordSocketClient();
+		await client.LoginAsync(Discord.TokenType.Bot, "MTA4MDIyOTAwNzA3MDYwMTIzNg.Gr03y-.xLihbloMPY6DVgbGGR6FtKx3JxMWzUSlZBclTY");
+		await client.StartAsync();
+
+		// send message
+		client.Ready += async () =>
+		{
+			var emojiName = new string[] { ":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":keycap_ten:" };
+
+			var guild = client.GetGuild(929708786027999262);
+			var channel = guild.GetTextChannel(1057602032850186300);
+			void GenerateFieldValue(IEnumerable<(string repo, int change, int oldIndex)> list, EmbedBuilder eb)
+			{
+				for (var i = 0; i < 10; i++)
+				{
+					var repo = list.ElementAt(i).repo;
+					var oldIndex = list.ElementAt(i).oldIndex;
+					var modDBEntry = databaseJson.releases.First(y => y.repo == repo);
+
+					var slug = modDBEntry.slug;
+					var modname = modDBEntry.name;
+					var change = list.ElementAt(i).change;
+					var uniqueName = modDBEntry.uniqueName;
+
+					var rankingEmoji = "";
+				
+					if (oldIndex == i)
+					{
+						rankingEmoji = "➖";
+					}
+					else if (oldIndex < i && oldIndex != -1)
+					{
+						rankingEmoji = "<:red_down:1080545078197637240>";
+					}
+					else
+					{
+						rankingEmoji = "<:green_up:1080545075005755543>";
+					}
+
+					eb.AddField($"{rankingEmoji} {emojiName[i]} {modname}", $"+{change} downloads [Mod Page](https://outerwildsmods.com/mods/{slug}/)");
+				}
+			}
+
+			List<Embed> embeds = new();
+
+			var otherInfo = new EmbedBuilder
+			{
+				Title = "General Statistics",
+				Color = Color.Orange,
+				Description = $"<:newhere:1079777473585229875> New Mods : {numberOfNewMods}{Environment.NewLine}" +
+				$"📋 Total Mods : {databaseJson.releases.GroupBy(x => x.uniqueName).Select(x => x.First()).Count()}"
+			};
+			embeds.Add(otherInfo.Build());
+
+			var topTenBuilder = new EmbedBuilder
+			{
+				Title = "Most Downloads",
+				Description = "This ranking does not include any mod tagged with `library`.",
+				Color = Color.Orange
+			};
+			GenerateFieldValue(topTen, topTenBuilder);
+			embeds.Add(topTenBuilder.Build());
+
+			await channel.SendMessageAsync($"Statistics from {DateTime.Now.Date.AddDays(-7).ToLongDateString()} to {DateTime.Now.ToLongDateString()}.", embeds: embeds.ToArray());
+
+			client.LogoutAsync();
+
+			Environment.Exit(0);
+		};
+
+		await Task.Delay(-1);
 	}
 
 	public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
 	{
-		// Unix timestamp is seconds past epoch
-		DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+		var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 		dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
 		return dateTime;
 	}
